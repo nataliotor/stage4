abd0644082823d7541863b96f3a2ecee0a359913
Natalli Otroshko <natalliotroshko@gmail.com>
2023-03-30 23:35:16 +0300

    TODO ->id>task     //TODO -> LocalDate from -> LocalDate to

--- src/main/java/com/home_09_03_2023/Task.java
+++ src/main/java/com/home_09_03_2023/Task.java
@@ -50,18 +50,26 @@ public class Task {
     }
 
     public int generateId() {
-        Random rnd = new Random();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
-            int number = rnd.nextInt(10);
-            stringBuilder.append(number);
+        Random rnd = new Random(); // створюємо Random для генерації рандомних чисел
+        StringBuilder stringBuilder = new StringBuilder(); // створюємо об'ект stringBuilder, який викорустовуємо для збирання чисел
+        for (int i = 0; i < 6; i++) { // починаємо цикл, який повторюється 6 разів
+            int number = rnd.nextInt(10); // генеруємо випадкове число від 0 до 9, та зберігаємо його в number
+            stringBuilder.append(number); // додаємо згенеровано число number до stringBuilder
 
         }
 
-       return Integer.parseInt(stringBuilder.toString());
+       return Integer.parseInt(stringBuilder.toString()); // повертаємо число, конвертувавши його з String до int, виористовуючи метод parseInt
     }
 
 
+
 }
+// append - це метод класу StringBuilder, який використовується для додавання рядків або символів до кінця поточного рядка, що вже зберігається у змінній StringBuilder.
+
+// Наприклад, якщо ми маємо об'єкт StringBuilder зі значенням "hello", ми можемо додати до нього інші рядки чи символи, використовуючи метод append(). Наприклад, stringBuilder.append(" world"); додаст до поточного рядка рядок " world", так що stringBuilder.toString() поверне "hello world".
 
 
+// Integer.parseInt - це статичний метод класу Integer, який використовується для перетворення рядка, що містить ціле число, в еквівалентне ціле число типу int. Цей метод виконує ті самі операції, що й метод parseInt, але знаходиться в класі Integer.
+//Синтаксис методу Integer.parseInt виглядає так: Integer.parseInt(String s). Де s - рядок, який містить ціле число, яке потрібно перетворити на тип int.
+//Наприклад, якщо ми маємо рядок "123", ми можемо використати Integer.parseInt("123") для отримання числового значення 123 у вигляді цілого числа типу int.
+//Якщо рядок не можна сконвертувати в ціле число, метод parseInt() згенерує помилку NumberFormatException. Наприклад, якщо ми спробуємо використати Integer.parseInt("abc"), буде згенерована помилка, оскільки "abc" не може бути перетворено в ціле число.
--- src/main/java/com/home_09_03_2023/TaskContainer.java
+++ src/main/java/com/home_09_03_2023/TaskContainer.java
@@ -8,26 +8,51 @@ import java.util.Comparator;
 import java.util.Date;
 
 
-
 public class TaskContainer {
     private ArrayList<Task> tasks = new ArrayList<>();
 
     public void createdTask(String topic, String description, LocalDateTime due, String assigned) { // метод створити завдання, який бере на вхід данні
-        Task mathematics = new Task(topic,description,due, assigned); // створюємо новий об'ект з параметрами
+        Task mathematics = new Task(topic, description, due, assigned); // створюємо новий об'ект з параметрами
         tasks.add(mathematics); // двізьми tasks та додай до нього новий об'ект mathematics
 
+
     }
 
-    public void readTask(String name) { // прочитай завдання
-        for (Task some : tasks) { //  проходимося по циклу
-            if (some.getTopic().equals(name)) { // якщо some.getTopic співпадае name
-                System.out.println("Task " + some.getTopic()); // надрукуй це завдання
+    //TODO ->id>task
+    //TODO -> LocalDate from -> LocalDate to
+//    public void readTask(String name) { // прочитай завдання
+//        for (Task some : tasks) { //  проходимося по циклу
+//            if (some.getTopic().equals(name)) { // якщо some.getTopic співпадае name
+//                System.out.println("Task " + some.getTopic()); // надрукуй це завдання
+//            }
+//        }
+//        System.out.println("No found"); // якщо такої назви немає, напиши, що не знайшов
+//    }
+    public void readTaskId(int id) { // метод прочитай завдання, з вхідним параметром id
+        for (Task task : tasks) { // проходимося по циклу
+            if (task.generateId() == id) { // якщо завдання з генерованим id співпадає з заданим id
+                System.out.println("Task Id " + task.generateId()); // то, надрукуй його
+//
+            } else {
+                System.out.println("No task with ID " + id); // то, напиши, що завдання не знайдено
+
             }
         }
-        System.out.println("No found"); // якщо такої назви немає, напиши, що не знайшов
+
     }
 
-    public void updateTask() { // метод оновити завдання (порахуй букви)
+    public void readTaskBetween(LocalDateTime created, LocalDateTime finished) { // метод який шукає завдання, які знаходяться між двома датами
+        ArrayList<Task> result = new ArrayList<>(); // створюємо ArrayList result, де будемо зберігати завдання
+        for (Task task : tasks) { // проходимося по масиву
+            LocalDateTime due = task.getDue(); // змінній due присовоюємо значання getDue, яке ми отримали з task
+            if (due != null && due.isAfter(created) && due.isBefore(finished)) { // якщо due не равна null та не знаходится між двома даиами (isAfter(created) && due.isBefore(finished))
+                result.add(task); // то додай завдання до списку
+            }
+        }
+    }
+
+
+        //public void updateTask () { // метод оновити завдання (порахуй букви)
 //        int current = 0; // значення дорівнює 0
 //        for (Task some : tasks) { // пройдемося по циклу
 //            if (Character.isAlphabetic(tasks.size())) { // якщо символ в tasks є буквою - порахуй його
@@ -39,7 +64,6 @@ public class TaskContainer {
 //        System.out.println();
 
 
-    }
 
     public void deleteTask() { // видалити завдання
         LocalDate finished = LocalDate.of(2023, 03, 12);
@@ -63,7 +87,7 @@ public class TaskContainer {
         int result = secondAuthor.length() - firstAuthor.length(); // определяем разницу между длиной авторов, это разрешит нам отссортировать строки по увелечению
 
         if (result == 0) { // если результат этих авторов имеют одинаковую длину
-            result =firstAuthor.compareTo(secondAuthor); /// сравни Author1 и Author2 и отсортируй по алфавиту
+            result = firstAuthor.compareTo(secondAuthor); /// сравни Author1 и Author2 и отсортируй по алфавиту
         }
         return result;
     }
